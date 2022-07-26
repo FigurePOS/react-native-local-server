01f0f550e0bbbf3dc2d293d5184a3e2829dd190e
David Lang <David.lang398@gmail.com>
2022-07-26 10:29:26 +0200

    chore: release.sh updated

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
+echo "Releasing SDK"
+
 dirname="$(dirname ${BASH_SOURCE[0]})"
 version=$(cat "$dirname/../package.json" | grep version | head -1 | awk -F= "{ print $2 }" | sed 's/[version:,\",]//g' | tr -d '[[:space:]]')
 
+if [ -z "$version" ]; then
+  echo "Failed to get version from package.json."
+  echo "dirname: $dirname"
+  echo "version: $version"
+  exit 1
+fi
+
 LAST_TAG="v$version"
+
+echo "Comparing files with the latest tag: $LAST_TAG"
 
 CHANGED_FILES="$(git diff --name-only "$LAST_TAG" "HEAD")"
 
@@ -11,7 +22,9 @@ ANDROID_FILES="$(echo  "$CHANGED_FILES" | grep "^android/src/main/java")"
 IOS_FILES="$(echo  "$CHANGED_FILES" | grep "^ios/")"
 
 if [ -z "$ANDROID_FILES" ] && [ -z "$IOS_FILES" ]; then
+  echo "No native files changed, releasing TS version only."
   yarn release:ts --ci
 else
+  echo "Native files changed, releasing new NATIVE version."
   yarn release:native --ci
 fi
