d76a909bb67ca773ae8b753a90a357ad661dde05
David Lang <David.lang398@gmail.com>
2023-07-19 14:34:38 +0200

    chore: update react native to v0.72.3

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,3 +1,5 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
+
 const path = require("path")
 const blacklist = require("metro-config/src/defaults/blacklist")
 const escape = require("escape-string-regexp")
@@ -9,7 +11,7 @@ const modules = Object.keys({
     ...pak.peerDependencies,
 })
 
-module.exports = {
+const config = {
     projectRoot: __dirname,
     watchFolders: [root],
 
@@ -33,3 +35,5 @@ module.exports = {
         }),
     },
 }
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config)
