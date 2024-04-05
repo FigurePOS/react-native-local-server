6a6fdd688f535a76281cf5dc45c07dddcb8013f0
David Lang <David.lang398@gmail.com>
2024-04-05 06:02:05 -0400

    fix: wip

--- src/messaging/server/operators/fromMessagingServerDataReceived.ts
+++ src/messaging/server/operators/fromMessagingServerDataReceived.ts
@@ -1,9 +1,19 @@
-import { Observable } from "rxjs"
+import { EMPTY, Observable, of } from "rxjs"
 import { DataObject } from "../../types"
 import { TCPServer } from "../../../"
-import { map } from "rxjs/operators"
+import { catchError, map, mergeMap } from "rxjs/operators"
 import { parseDataObject } from "../../functions/parseDataObject"
 import { fromTCPServerEvent } from "../../../tcp/server/operators/"
 
 export const fromMessagingServerDataReceived = (serverId: string): Observable<DataObject> =>
-    fromTCPServerEvent(serverId, TCPServer.EventName.DataReceived).pipe(map(parseDataObject))
+    fromTCPServerEvent(serverId, TCPServer.EventName.DataReceived).pipe(
+        mergeMap((data) =>
+            of(data).pipe(
+                map(parseDataObject),
+                catchError(() => {
+                    // log error
+                    return EMPTY
+                })
+            )
+        )
+    )
