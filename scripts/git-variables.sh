87c395d014c2c4a3fe163d49c2d22af4d180c9d5
Krcma <49247463+Krcma@users.noreply.github.com>
2026-02-04 13:15:35 +0100

    feat: switch to GHA from Circle CI FGR3-8438 (#86)
    * feat: switch to GHA from Circle FGR3-8438
    
    * feat: disable circle and lint check FGR3-8438
    
    * feat: try to force use tsconfig.build.json FGR3-8438
    
    * feat: run tests FGR3-8438
    
    * feat: update podfile.lock FGR3-8438
    
    * feat: add changesets FGR3-8438
    
    * feat: init changesets FGR3-8438
    
    * feat: fix format FGR3-8438
    
    * feat: delete unused script FGR3-8438
    
    * Update changeset to reflect CI switch
    
    * feat: review comments FGR3-8438

--- scripts/git-variables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Setting git config:"
-
-echo "email: $GITHUB_USER_EMAIL"
-git config --global user.email "$GITHUB_USER_EMAIL"
-
-echo "name: $GITHUB_USER_NAME"
-git config --global user.name "$GITHUB_USER_NAME"
