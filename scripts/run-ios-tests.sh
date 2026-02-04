ae34701039bc9bdc6bae3837e9f91abb49685c84
krcma <krcma.jan@seznam.cz>
2026-02-04 14:23:12 +0100

    feat: fail when ios tests fail 4 FGR3-8438

--- scripts/run-ios-tests.sh
+++ scripts/run-ios-tests.sh
@@ -3,12 +3,15 @@
 echo "Running iOS unit tests."
 
 set -e  # Exit immediately if a command exits with a non-zero status
+set -o pipefail  # Ensure that if xcodebuild fails, the entire pipeline fails
 
 xcodebuild \
   -project ios/LocalServer.xcodeproj \
   -scheme Tests \
-  test
+  test \
+  | xcbeautify
 
 echo "✅ iOS tests completed successfully"
+
 
 
