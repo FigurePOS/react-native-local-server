80763dd338d53d113c8bba5ce522295d7b51ff14
krcma <krcma.jan@seznam.cz>
2026-02-09 12:36:31 +0100

    fix: console.logs FGR3-8440

--- src/utils/logger/logger.ts
+++ src/utils/logger/logger.ts
@@ -2,12 +2,15 @@ import { Logger } from "./types"
 
 export const DefaultLogger: Logger = {
     log: (message, data) => {
+        // eslint-disable-next-line no-console
         console.log(message, data)
     },
     warn: (message, data) => {
+        // eslint-disable-next-line no-console
         console.warn(message, data)
     },
     error: (message, data) => {
+        // eslint-disable-next-line no-console
         console.error(message, data)
     },
 }
