ccf2301167d210a6bd4c4b06c35049789a3199d3
krcma <krcma.jan@seznam.cz>
2026-02-09 09:55:11 +0100

    feat: eslint.config.mjs FGR3-8440

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import figurePlugin from "@figuredev/eslint-plugin-react-native"
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    // This wild card enables to pickup the config in nested directories it is perfect for this kind of repo,
+    // but would be a huge bottleneck in performance for our repos containing apps,
+    // where there is only one tsconfig.json, so please don't use it there.
+    ...figurePlugin("**/tsconfig.json"),
+    {
+        files: ["apps/example-expo/**"],
+        rules: {
+            // we want to use console in the example app, to easily debug
+            "no-console": "off",
+        },
+    },
+    {
+        ignores: ["**/__tests__/**", "**/__mocks__/**", "**/__rsc_tests__/**"],
+    },
+]
