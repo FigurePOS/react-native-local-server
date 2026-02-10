2f6f9ffd937573dadd9e971d7639bb9f0fc318d7
krcma <krcma.jan@seznam.cz>
2026-02-10 10:50:36 +0100

    fix: fromEventFixed ignores FGR3-8440

--- example/src/common/utils.ts
+++ example/src/common/utils.ts
@@ -1,7 +1,9 @@
 import { Observable, Subscriber } from "rxjs"
 
 // We use this because rxjs fromEvent calls deprecated method on event emitter
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const fromEventFixed = (eventEmitter: any, eventName: string) =>
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     new Observable((subscriber: Subscriber<any>) => {
         if (eventEmitter.addListener != null) {
             const subscription = eventEmitter.addListener(eventName, (event: unknown) => {
--- src/utils/operators/fromEventFixed.ts
+++ src/utils/operators/fromEventFixed.ts
@@ -3,7 +3,9 @@ import { Observable, Subscriber } from "rxjs"
 import { ErrorWithMetadata } from "../errors"
 
 // We use this because rxjs fromEvent calls deprecated method on event emitter
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const fromEventFixed = <T>(eventEmitter: any, eventName: string): Observable<T> =>
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     new Observable((subscriber: Subscriber<any>) => {
         if (eventEmitter.addListener != null) {
             const subscription = eventEmitter.addListener(eventName, (event: unknown) => {
