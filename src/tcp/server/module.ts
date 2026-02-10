9f0a1d8e5ab81fc49b07fd8beb6d98a4c7ccfd16
krcma <krcma.jan@seznam.cz>
2026-02-10 09:43:57 +0100

    fix: formatting 2 FGR3-8440

--- src/tcp/server/module.ts
+++ src/tcp/server/module.ts
@@ -1,10 +1,11 @@
 import { NativeModules, Platform } from "react-native"
 
-const LINKING_ERROR =
-    `The package 'react-native-local-server' doesn't seem to be linked. Make sure: \n\n${ 
-    Platform.select({ ios: "- You have run 'pod install'\n", default: "" }) 
-    }- You rebuilt the app after installing the package\n` +
-    `- You are not using Expo managed workflow\n`
+const LINKING_ERROR = `The package 'react-native-local-server' doesn't seem to be linked. Make sure: \n\n${Platform.select(
+    {
+        ios: "- You have run 'pod install'\n",
+        default: "",
+    },
+)}- You rebuilt the app after installing the package\n- You are not using Expo managed workflow\n`
 
 export const TCPServerModule = NativeModules.TCPServerModule
     ? NativeModules.TCPServerModule
