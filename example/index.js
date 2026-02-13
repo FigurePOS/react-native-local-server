f04bcb19024030d225907c3447feb2731df78e4e
krcma <krcma.jan@seznam.cz>
2026-02-13 08:25:45 +0100

    feat: run lint and ts checks for root and example FGR3-8440

--- example/index.js
+++ example/index.js
@@ -3,3 +3,5 @@ import { registerRootComponent } from "expo"
 import { App } from "./src/App"
 
 registerRootComponent(App)
+
+// dummy component to render - check if it renders, if yes, go one component lower and so on
