a95c4ad4962865ed4a57b02944a731db4de2aaed
krcma <krcma.jan@seznam.cz>
2026-02-13 08:27:39 +0100

    feat: lol FGR3-8440

--- example/index.js
+++ example/index.js
@@ -3,5 +3,3 @@ import { registerRootComponent } from "expo"
 import { App } from "./src/App"
 
 registerRootComponent(App)
-
-// dummy component to render - check if it renders, if yes, go one component lower and so on
